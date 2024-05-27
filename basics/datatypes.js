f2db34c5808b6eeeb6243c5b63ed32aeb8fdfa46
ShadabZeni <shadab.raza@zeni.ai>
2024-05-27 06:18:44 +0000

    new datatype added

---
+++ basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all JS code as newer version
+
+let name =  "zeni"
+let age = 10
+let isnum = false
+
+console.log( typeof "age")
---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const account_id = 123
+let accountEmail = "shadab1@gmail.com"
+var accountPassword = "12345"
+acoountCity = "jaipur"
+let accountState;
+
+accountEmail = "zeni@gmail.com"
+accountPassword = "54321"
+acoountCity = "Delhi"
+
+// account_id = 2 // dont use it.
+
+/*
+dont use var
+*/
+
+// console.log(account_id);
+
+console.table([account_id, accountEmail, accountPassword,acoountCity, accountState])
